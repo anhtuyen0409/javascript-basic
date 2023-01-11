198f8faf60eaad91f2472178d10a80f54b02d031
Nguyen Anh Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-01-11 10:56:44 +0700

    how to using template strings in js

---
+++ lesson6/lesson6.js
@@ -0,0 +1,12 @@
+//learning tempalte strings in js
+let a2 = 5;
+let b2 = 10;
+//not template strings
+console.log('Sum is ' + (a2 + b2) + '\nExpression is ' + (2 * a2 + b2))
+//using template strings
+//note: using ` replace for '
+console.log(`Value of variable a2 is: ${a2} and b2 = ${b2} 
+a2 + b2 = ${a2+b2}
+2 * a2 + b2 = ${2 * a2 + b2}`)
+//url: nguyenanhtuyen/5
+console.log(`nguyenanhtuyen/${a2}`)
