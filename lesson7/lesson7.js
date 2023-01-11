3799f56db33df52ceb30893088a490e0c8ce7f91
Nguyen Anh Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-01-11 11:26:18 +0700

    learning object in js

---
+++ lesson7/lesson7.js
@@ -0,0 +1,17 @@
+//learning object in js
+//using {} to declare object
+//khởi tạo 1 object có 3 thuộc tính: name, address, age
+let obj = {
+    name: 'Tuyen',
+    address: 'Binh Dinh',
+    age: 22
+}; //object
+//lấy giá trị các thuộc tính trong object
+console.log('My name is ',obj.name)
+console.log('My address is ',obj.address)
+console.log('I am ',obj.age)
+
+let b3 = 'hello'; //string
+//check
+console.log('type of a3: ',typeof obj)
+console.log('type of b3: ',typeof b3)
