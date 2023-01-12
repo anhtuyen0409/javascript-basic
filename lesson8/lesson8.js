24eaaa63cbc7cfd2c09e5ff67b5eb91e373466cd
Nguyen Anh Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-01-12 10:56:17 +0700

    learning array in js

---
+++ lesson8/lesson8.js
@@ -0,0 +1,17 @@
+//khoi tao 1 object
+let obj1 = {
+    name : "Tuyen",
+    age: 22
+};
+console.log('Check value object: ',obj1)
+//create arraay
+let arr = ['Manchester City','Chelsea','Liverpool'];
+//check
+console.log('Type of arr: ',typeof arr,' and value of arr: ',arr)
+//ban chat array cung la object
+//array quản lí các phần tử trong nó theo index (index chạy từ 0,1,2...)
+//check
+//lấy phần tử đầu tiên
+console.log('First item: ',arr[0])
+//lấy phần tử thứ 2
+console.log('Second item: ',arr[1])
