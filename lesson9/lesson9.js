0151e2de04620fa47c12da3b1e6df22811161785
Nguyen Anh Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-01-12 11:53:18 +0700

    compare == and === in js

---
+++ lesson9/lesson9.js
@@ -0,0 +1,15 @@
+//learing operator == vs ===
+//create 4 avarible
+let a9 = 5;
+let b9 = 5;
+let c9 = '5';
+let d9 = 4;
+console.log(a9==b9) //true
+console.log(a9==c9) //true
+console.log(a9==d9) //false
+//=> toán tử == chỉ so sánh về mặt giá trị, nó không quan tâm đến kiểu dư liệu
+//sử dụng ===
+console.log(a9===b9) //true
+console.log(a9===c9) //false
+console.log(a9===d9) //false
+//=> toán tử === so sánh về giá trị và cả về kiểu dữ liệu 
