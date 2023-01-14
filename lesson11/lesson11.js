57a8b4216b2cb034701dba1520182f52d4264cf8
Nguyen Anh Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-01-14 22:06:40 +0700

    how to using while and do while loop in js

---
+++ lesson11/lesson11.js
@@ -0,0 +1,13 @@
+//learning while and do...while in js
+let arr11 = ['liverpool','man city','arsenal','chelsea'];
+let i = 0;
+while(i < arr11.length){
+    console.log('Top ',i+1,': ',arr11[i])
+    i++
+}
+
+let j = 0;
+do{
+    console.log('Top ',j+1,': ',arr11[j])
+    j++
+}while(j < arr11.length);
