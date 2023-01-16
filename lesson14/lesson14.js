261da703d54d5164d6e3a21941fd12ea23f203e2
Nguyen Anh Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-01-16 21:24:15 +0700

    learning function in js

---
+++ lesson14/lesson14.js
@@ -0,0 +1,6 @@
+//learning avaribles & block scope
+//2 type avarible
+//global
+//block
+//nên dùng let thay var
+//let sẽ siết chặt phạm vi của biến
---
+++ lesson15/lesson15.js
@@ -0,0 +1,7 @@
+//learning function in js
+//declare function in js
+function sum(a,b){
+    return a + b;
+}
+//check result
+console.log('a + b = ',sum(2,3))
