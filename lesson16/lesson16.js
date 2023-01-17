daf959a15daebe874cfb40be647e3be00b162abd
Nguyen Anh Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-01-17 12:53:04 +0700

    learning arrow function in js

---
+++ lesson16/lesson16.js
@@ -0,0 +1,9 @@
+//learing arrow function in js
+//create arrow function
+//using =>
+let sum = (a,b) =>{
+    return a + b;
+}
+//check result
+console.log('sum a + b = ',sum(2,3));
+//su dung arrow function ta co the lien tuong nhu su dung bien nen su dung let
